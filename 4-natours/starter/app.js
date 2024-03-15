1673800b1eb34ef3b19a4d0e1829073361b86469
Landon Cuff <landoncuff23@gmail.com>
2024-03-14 19:26:39 -0600

    Installed Express, package.json and Started routing

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// Will add a bunch of methods into the app variable
+const app = express();
+
+// Routing to get HTTP method
+app.get('/', (req, res) => {
+  res
+    .status(400)
+    .json({ message: 'Hello from the server side', app: 'Natour' });
+});
+
+app.post('/', (req, res) => {
+    res.send("You can post to this endpoint");
+});
+
+const port = 3000;
+// Will start up a server
+app.listen(port, () => {
+  console.log('App Running on port ' + port);
+});
