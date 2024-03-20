43551f34155113fef58d71a41558923044cd8736
Landon Cuff <landoncuff23@gmail.com>
2024-03-19 20:22:16 -0600

    PATCH & DELETE request

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -29,18 +29,18 @@ app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1; // will convert string into number
   const tour = tours.find((el) => el.id === id);
 
-  if(!tour){
+  if (!tour) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID'
+      message: 'Invalid ID',
     });
   }
 
   res.status(200).json({
     status: 'success',
     data: {
-      tour: tour
-    }
+      tour: tour,
+    },
   });
 });
 
@@ -63,6 +63,38 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+// Updating the data
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here>',
+    },
+  });
+});
+
+// Delete data 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 const port = 3000;
