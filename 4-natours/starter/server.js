c42b6b21a21ef5e1a421f97d65e5482146a7a5b4
Landon Cuff <landoncuff23@gmail.com>
2024-03-27 19:00:48 -0600

    ESLint Install

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,12 +1,11 @@
 const dotenv = require('dotenv');
 // Getting our environment variables
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 // Will start up a server
 app.listen(port, () => {
-  console.log('App Running on port ' + port);
+  // console.log('App Running on port ' + port);
 });
-
