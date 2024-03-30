3a5f973c60ab435d0a3cac6d06a7ef2a39e0cffe
Landon Cuff <landoncuff23@gmail.com>
2024-03-29 18:49:14 -0600

    Created MongoDB database

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -4,7 +4,8 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 27017;
 // Will start up a server
 app.listen(port, () => {
   // console.log('App Running on port ' + port);
