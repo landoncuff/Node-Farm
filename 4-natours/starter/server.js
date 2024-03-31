066a3fa2dd67b20be3b2d274b29610e4fed0a9f7
Landon Cuff <landoncuff23@gmail.com>
2024-03-30 20:04:45 -0600

    Connected Database

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,11 +1,25 @@
+// Getting Access to our database through mongoose
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 // Getting our environment variables
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => { // Returns a Promise
+  console.log('DB Connection successful');
+});
+
 const app = require('./app');
 
-// const port = process.env.PORT || 3000;
-const port = 27017;
+const port = process.env.PORT || 3000;
 // Will start up a server
 app.listen(port, () => {
   // console.log('App Running on port ' + port);
