b5715d901849b4d6f7d152d1bb58d96dbd2a5d7a
Landon Cuff <landoncuff23@gmail.com>
2024-04-02 19:28:33 -0600

    Create new tour

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('./../models/tourModel'); // Getting our model
 
-exports.checkBody = function (req, res, next) {
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Missing name and price',
-    });
-  }
-  next();
-};
-
 //! ROUTES
 
 exports.getAllTours = (req, res) => {
@@ -36,13 +26,21 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
