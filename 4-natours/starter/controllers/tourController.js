7a55691fa90c650fa0ab38f23f9b6a7d7c631558
Landon Cuff <landoncuff23@gmail.com>
2024-04-15 19:23:28 -0600

    Refactored API

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,5 +1,6 @@
 const { Linter } = require('eslint');
 const Tour = require('./../models/tourModel'); // Getting our model
+const APIFeatures = require('./../utils/apiFeatures')
 
 //! ROUTES
 
@@ -12,52 +13,14 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // BUILDING QUERY
-    // 1) FILTERING
-    const queryObj = { ...req.query }; // Making a shallow copy of queries
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    // 2) ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj); // Logic operators
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr)); // Returns a query
-
-    // 3) SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      // Default sortby
-      query = query.sort('-createdAt _id');
-    }
-
-    // 4) Limiting fields
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      // Excluding mongoose default values
-      query = query.select('-__v');
-    }
-
-    // 5) Pagination
-    const page = req.query.page * 1 || 1; // turning string into number but default to 1
-    const limit = req.query.limit * 1 || 100; // turning string into number but default to 100
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) {
-        throw new Error('This page does not exist');
-      }
-    }
-
     // EXECUTE QUERY
-    const tours = await query;
+    // We can only chain because in each method we return the "this" object
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // SENDING QUERY
     res.status(200).json({
