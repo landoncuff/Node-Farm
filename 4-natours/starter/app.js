b538ca7ee70c3887692e1e4cb50dc417a4dad606
Landon Cuff <landoncuff23@gmail.com>
2024-05-04 20:21:47 -0600

    Handle wrong routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 //! Middlewares
 // 3rd Party morgan
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
@@ -19,12 +19,6 @@ app.use(express.json());
 
 // Allowing access to static files -- middleware
 app.use(express.static(`${__dirname}/public`));
-
-// Building our own middleware
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -35,7 +29,15 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//! Middleware that will handle routes that do not exist
+// .all will handle all the urls that are not handled
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'Fail',
+    message: `Can't find ${req.originalUrl} on the server!`,
+  });
+  next();
+});
+
 //! START SERVER
 module.exports = app;
-
-// Database work
