9f3c0c4e95291f145a85a5b26394fcafee5f3e7a
Landon Cuff <landoncuff23@gmail.com>
2024-09-10 18:13:48 -0600

    Handling Uncaught Exception and unhandled rejection

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -2,6 +2,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION');
+  console.log(err.name, err.message);
+  // Shutting down the app
+  process.exit(1);
+});
+
 // Getting our environment variables
 dotenv.config({ path: './config.env' });
 
@@ -25,6 +32,17 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 // Will start up a server
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // console.log('App Running on port ' + port);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name);
+  console.log(err.message);
+  console.log('UNHANDLED REJECTION');
+  // Closing server before shutting down app
+  server.close(() => {
+    // Shutting down the app
+    process.exit(1);
+  });
 });
